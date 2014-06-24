19470c5c55ded0b3205bb5f0430398210ab9194f
Daniel Catipon <daniel.catipon@gmail.com>
2014-06-24 13:46:23 -0700

    Move code from root.rb and deleted it

--- lib/math_root_helper.rb
+++ lib/math_root_helper.rb
@@ -1,6 +1,19 @@
 require "math_root_helper/version"
-require "math_root_helper/root"
 
 module MathRootHelper
-  # Your code goes here...
+  def self.square_root(num)
+    (num**(1.0/2)).round(14)
+  end
+
+  def self.cube_root(num)
+    (num**(1.0/3)).round(14)
+  end
+
+  def self.nth_root(n, num)
+    if n == 0
+      "Infinity"
+    else
+      (num**(1.0/n)).round(14)
+    end
+  end
 end
