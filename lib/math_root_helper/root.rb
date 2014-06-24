f5ce72a03c177f2ac588d08cf47dcd418f5cc9e7
Daniel Catipon <daniel.catipon@gmail.com>
2014-06-24 12:27:07 -0700

    Modify math_root_helper module

--- lib/math_root_helper/root.rb
+++ lib/math_root_helper/root.rb
@@ -1,19 +1,17 @@
 module MathRootHelper
-  class Root
-    def self.square(num)
-      num**(1.0/2)
-    end
+  def self.square_root(num)
+    num**(1.0/2)
+  end
 
-    def self.cube(num)
-      num**(1.0/3)
-    end
+  def self.cube_root(num)
+    num**(1.0/3)
+  end
 
-    def self.nth(n, num)
-      if n == 0
-        "Infinity"
-      else
-        num**(1.0/n)
-      end
+  def self.nth_root(n, num)
+    if n == 0
+      "Infinity"
+    else
+      num**(1.0/n)
     end
   end
 end
--- spec/math_root_helper_spec.rb
+++ spec/math_root_helper_spec.rb
@@ -1,29 +1,29 @@
 require 'math_root_helper'
 
-describe MathRootHelper::Root do
+describe MathRootHelper do
 
   it "the square root of '0'" do
-    MathRootHelper::Root.square(0).should == 0
+    MathRootHelper.square_root(0).should == 0
   end
 
   it "the square root of '4'" do
-    MathRootHelper::Root.square(4).should == 2
+    MathRootHelper.square_root(4).should == 2
   end
 
   it "the cube root of '0'" do
-    MathRootHelper::Root.cube(0).should == 0
+    MathRootHelper.cube_root(0).should == 0
   end
 
   it "the cube root of '8'" do
-    MathRootHelper::Root.cube(8).should == 2
+    MathRootHelper.cube_root(8).should == 2
   end
 
   it "the 0 root of '7'" do
-    MathRootHelper::Root.nth(0, 7).should == 'Infinity'
+    MathRootHelper.nth_root(0, 7).should == 'Infinity'
   end
 
   it "the 4th root of '16'" do
-    MathRootHelper::Root.nth(4, 16).should == 2
+    MathRootHelper.nth_root(4, 16).should == 2
   end
 
 end
